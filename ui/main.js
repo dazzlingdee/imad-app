825024c1fc31f5b6f67613d695686cd77bd4ca19
dazzlingdee <deekshabd1@gmail.com>
2017-08-15 11:34:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 var img=document.getElementById('hey');
-hey.onclick=function(){
-    hey.style.marginLeft=100px;
+img.onclick=function(){
+    img.style.marginLeft=100px;
 };
