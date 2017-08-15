1aef876c1ee0e98a85bcf8cfe40dd968f3a53c4d
dazzlingdee <deekshabd1@gmail.com>
2017-08-15 11:45:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var img=document.getElementById('hey');
 var marginLeft=0;
 function moveRight(){
     
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+3;
     img.style.marginLeft=marginLeft+'px';
 };
 img.onclick=function(){
-    var interval=setInterval(moveRight,10)
+    var interval=setInterval(moveRight,100)
    // img.style.marginLeft='500px';
 };
