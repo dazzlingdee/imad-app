2e60aee69c04dbdcf281ffb5f88ac538a495882c
dazzlingdee <deekshabd1@gmail.com>
2017-08-15 13:02:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,7 @@ var name=nameInput.value;
 var submit=documnet.getElementById('submit-button');
 submit.onclick=function(){
     var names=['name1','name2'];
+    var list='';
     for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
