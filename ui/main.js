1120cc2ec45f72874274b5e5e7560a44b5389f41
dazzlingdee <deekshabd1@gmail.com>
2017-08-15 12:02:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
 console.log('Loaded!');
-var img=document.getElementById('hey');
-var marginLeft=0;
-function moveRight(){
-    
-    marginLeft=marginLeft+3;
-    img.style.marginLeft=marginLeft+'px';
+var button=getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
-img.onclick=function(){
-    var interval=setInterval(moveRight,100)
-   // img.style.marginLeft='500px';
-};
+
