2818eb3b03f4aceb24b9be9273d308e48ff73e97
dazzlingdee <deekshabd1@gmail.com>
2017-08-16 16:43:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,11 +19,22 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit-button');
 submit.onclick=function(){
-    var names=['name1','name2'];
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+                   var names=['name1','name2'];
     var list='';
     for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+            }
+    }
+    }
+    
+};
+    request.open('GET','http://deekshabd1.imad.hasura-app.io/counter',true);
+request.send(null);
 };
