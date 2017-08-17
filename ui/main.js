14f4afbee5491266771ec015e3e1f5f3fc8e0ccb
dazzlingdee <deekshabd1@gmail.com>
2017-08-17 06:21:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,6 +37,6 @@ submit.onclick=function(){
     var nameInput=document.getElementById('name');
 var name=nameInput.value;
     
-    request.open('GET','http://deekshabd1.imad.hasura-app.io/submit-name?name=+name',true);
+    request.open('GET','http://deekshabd1.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
 };
