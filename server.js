3e79ef9e89c2b4ffe13ea139577307b0146412f2
dazzlingdee <deekshabd1@gmail.com>
2017-08-24 11:29:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,17 +13,7 @@ var config{
 var app = express();
 app.use(morgan('combined'));
 
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,res){
-        if(err){
-            res.status(500).send(err,toString());
-        }
-        else{
-            res.status(JSON.stringify(result));
-        }
-    });
-});
+
 
 var names=[];
 app.get('/submit-name',function(req,res){
@@ -131,7 +121,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,res){
+        if(err){
+            res.status(500).send(err,toString());
+        }
+        else{
+            res.status(JSON.stringify(result));
+        }
+    });
+});
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
