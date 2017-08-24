43bf1e6d3a6c8ac0ee1444c39b3a33c18fcfe859
dazzlingdee <deekshabd1@gmail.com>
2017-08-24 11:13:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config{
+    user:'deekshabd1',
+    database:'deekshabd1',
+    host:'da.imad.hasura-app.io',
+    posr:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -110,8 +118,17 @@ app.get('/submit-name',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
-    
+    pool.query('SELECT * FROM test',function(err,res){
+        if(err){
+            res.status(500).send(err,toString());
+        }
+        else{
+            res.status(JSON.stringify(result));
+        }
+    });
 });
 var counter=0;
 app.get('/counter',function(req,res){
