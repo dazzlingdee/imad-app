bc524ebf9171c1a09183c189289744e199f2bb69
dazzlingdee <deekshabd1@gmail.com>
2017-08-24 11:55:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user: 'deekshabd1',
     database: 'deekshabd1',
     host: 'db.imad.hasura-app.io',
-    port: '5432',
+    port: '80',
     password:process.env.DB_PASSWORD
 };
 var app = express();
