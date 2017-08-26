2532e12bfd33217acc168457624af3de74d1f2cc
dazzlingdee <deekshabd1@gmail.com>
2017-08-26 10:39:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user: 'deekshabd1',
     database: 'deekshabd1',
     host: 'db.imad.hasura-app.io',
-    port: '80',
+    port: '8080',
     password:process.env.DB_PASSWORD
 };
 var app = express();
@@ -123,7 +123,7 @@ var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,res){
         if(err){
-            res.status(500).send(err,toString());
+            res.status(500).send(err.toString());
         }
         else{
             res.send(JSON.stringify(result));
